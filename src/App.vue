8edea4bdab78c74fbf314037d796107a25afe15a
Claudine LAFINE <claudine.lafine@gmail.com>
2021-05-24 18:14:50 +0200

    modifications consequences

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="indigo" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          :src="require('@/assets/logo4.png')"
+          transition="scale-transition"
+          width="60"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink hidden-sm-and-down"
+          contain
+          min-width="100"
+          :src="require('@/assets/ator-name.png')"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <Home />
+    </v-main>
+    <v-footer dark padless app>
+      <v-row justify="center" no-gutters>
+        <v-col class="indigo py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} -
+          <strong> Made by Claudine with <v-icon>mdi-cards-heart</v-icon></strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld,
+    Home,
   },
+
+  data: () => ({}),
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,137 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-dialog v-model="isLoading" persistent width="300" dark>
+        <div class="text-center">
+          <v-card color="indigo" dark>
+            <v-card-title>Converting... </v-card-title>
+            <v-card-text>
+              <v-progress-circular
+                indeterminate
+                size="70"
+                :width="7"
+                color="white"
+              ></v-progress-circular>
+            </v-card-text>
+          </v-card>
+        </div>
+      </v-dialog>
+    </v-row>
+
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img :src="require('../assets/romancharacter.svg')" class="my-1" contain height="300" />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to the AToR Converter !</h1>
+
+        <p class="subheading font-weight-regular">
+          Simply type in an arabic numeral from 1 to 100 and get it converted to it's roman numeral
+          equivalent.
+        </p>
+      </v-col>
+
+      <v-col v-if="convertedNumber.length > 0" class="mb-5" cols="12">
+        <v-row justify="center">
+          <v-alert
+            text
+            outlined
+            color="indigo"
+            icon="mdi-progress-check"
+            transition="scale-transition"
+          >
+            <v-row align="center">
+              <v-col class="grow">
+                <div class="title">Here's your answer :</div>
+                <p>{{ convertedNumber }}</p>
+              </v-col>
+              <v-col class="shrink">
+                <v-btn color="indigo" @click="handleClick" icon>
+                  <v-icon>mdi-thumb-up</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-alert>
+        </v-row>
+      </v-col>
+
+      <v-col class="mb-5" cols="12" v-show="!convertedNumber">
+        <h2 class="headline font-weight-bold mb-3">Enter your number here ...</h2>
+
+        <v-row justify="center">
+          <v-col cols="12" sm="6" md="3">
+            <v-form
+              class="d-flex flex-row flex-nowrap align-baseline justify-space-around"
+              ref="form"
+            >
+              <v-text-field
+                data-test="input"
+                v-model="value"
+                class="mx-6 py-2"
+                type="number"
+                label="Arabic numeral"
+                min="1"
+                max="100"
+                step="1"
+                :rules="valueRules"
+                @keydown.enter="submitForm"
+                required
+                outlined
+              ></v-text-field>
+              <v-btn color="indigo" fab dark class="py-2" @click="submitForm">Go !</v-btn>
+            </v-form>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import getRomanConversionService from '@/services/ConverterService.js'
+import axios from 'axios'
+export default {
+  name: 'Home',
+
+  data: () => ({
+    value: '',
+    isLoading: false,
+    convertedNumber: '',
+    valueRules: [
+      (v) => !!v || 'Value is required',
+      (v) => (v && /^[1-9][0-9]?$|^100$/gm.test(v)) || 'Value must be between 1 and 100',
+    ],
+  }),
+  methods: {
+    submitForm() {
+      const apiClient = axios.create({
+        baseURL: 'http://localhost:5000/rest/converter',
+        withCredentials: false,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      const ConverterService = getRomanConversionService(apiClient)
+      if (this.$refs.form.validate()) {
+        this.isLoading = true
+        ConverterService.getConvertedNumber({ input: this.value }).then((response) => {
+          this.convertedNumber = response.data
+          this.isLoading = false
+        })
+        this.$refs.form.reset()
+      }
+    },
+
+    handleClick() {
+      this.convertedNumber = ''
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.loading-dialog {
+  background-color: #303030;
+}
+</style>
